7200e88cfeb58d52734605414a4477274aebc6ee
ErubielFlota <santosflota8@gmail.com>
2019-05-13 03:09:15 -0500

    Logre avanzar hasta agregar los fragmentos. pag 21

--- app/src/main/java/com/example/navegacioninferior/MainActivity.java
+++ app/src/main/java/com/example/navegacioninferior/MainActivity.java
@@ -1,15 +1,60 @@
 package com.example.navegacioninferior;
 
+import android.support.design.widget.CoordinatorLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.view.MenuItem;
+
+
+import com.example.navegacioninferior.fragment.CartFragment;
+import com.example.navegacioninferior.fragment.GiftsFragment;
+import com.example.navegacioninferior.fragment.ProfileFragment;
+import com.example.navegacioninferior.fragment.StoreFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActionBar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        toolbar = getSupportActionBar();
 
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        toolbar.setTitle("Shop");
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragment;
+            switch (item.getItemId()) {
+                case R.id.navigation_shop:
+                    toolbar.setTitle("Shop");
+                    return true;
+                case R.id.navigation_gifts:
+                    toolbar.setTitle("My Gifts");
+                    return true;
+                case R.id.navigation_cart:
+                    toolbar.setTitle("Cart");
+                    return true;
+                case R.id.navigation_profile:
+                    toolbar.setTitle("Profile");
+                    return true;
+            }
+            return false;
+        }
+    };
 }
+
